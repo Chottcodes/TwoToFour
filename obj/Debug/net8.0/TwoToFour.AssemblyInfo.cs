ab177d6154824d8262636973dfff16f8cb1b2c5c
Chottcodes <cwellshott@gmail.com>
2025-01-07 15:32:39 -0800

    update

--- obj/Debug/net8.0/TwoToFour.AssemblyInfo.cs
+++ obj/Debug/net8.0/TwoToFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TwoToFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffffbfb861a98c659b14d39c76b61c30b366319d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17ddc07232b28797111882f32bac8140016cb8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TwoToFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TwoToFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
